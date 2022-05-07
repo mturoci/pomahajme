41ef022b9e03d3eedc314fb6b99d80856c8954a3
Martin Turoci <martinturoci@gmail.com>
2022-05-07 14:39:20 +0200

    feat: Landing page.

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Controllers\StoryController;
+
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\StoryController;
+use App\Story;
+use App\Transaction;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StoryController::class, 'index']);
+Route::get('/', function () {
+    return view('landing', [
+        'raised' => Transaction::where('amount', '>', 0)->sum('amount'),
+        'storyCount' => Story::count(),
+        'donors' => Transaction::distinct()->count('date')
+    ]);
+});
 Route::get('/pribehy', [StoryController::class, 'index']);
 Route::get('/pribehy/{id}', [StoryController::class, 'show']);
 Route::get('/statistika', [StoryController::class, 'stats']);
-Route::get('/euro-nadeje', function() { return view('euro'); });
-Route::get('/kontakt', function() { return view('contact'); });
+Route::get('/euro-nadeje', function () {
+    return view('euro');
+});
+Route::get('/kontakt', function () {
+    return view('contact');
+});
 Route::get('/admin', [StoryController::class, 'indexAdmin'])->middleware('auth');
-Route::get('/admin/login', function() { return view('login'); });
-Route::get('/admin/pribeh', function() { return view('admin-story', ['title' => 'Pridať príbeh', 'btn' => 'Vytvoriť']); })->middleware('auth');
+Route::get('/admin/login', function () {
+    return view('login');
+});
+Route::get('/admin/pribeh', function () {
+    return view('admin-story', ['title' => 'Pridať príbeh', 'btn' => 'Vytvoriť']);
+})->middleware('auth');
 Route::get('/admin/upravit-pribeh', [StoryController::class, 'update'])->middleware('auth');
 
 
