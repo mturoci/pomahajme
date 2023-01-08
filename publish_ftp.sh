a1d6d2edac5451ed148f6bb6a091653c28575474
Martin Turoci <martinturoci@gmail.com>
2023-01-08 13:49:12 +0100

    ci: Fix publish job by reconfiguring checkout action.

--- publish_ftp.sh
+++ publish_ftp.sh
@@ -1,7 +1,7 @@
-if [ -z "$FTP_HOST" ] || [ -z "$FTP_USER" ] || [ -z "$FTP_PASS" ]; then
-    echo "FTP_HOST, FTP_USER and FTP_PASS env variables must be set."
-    exit 1
-fi
+# if [ -z "$FTP_HOST" ] || [ -z "$FTP_USER" ] || [ -z "$FTP_PASS" ]; then
+#     echo "FTP_HOST, FTP_USER and FTP_PASS env variables must be set."
+#     exit 1
+# fi
 
 # Get last commit changed files.
 files=$(git diff --name-only HEAD^ HEAD)
@@ -17,9 +17,20 @@ for file in $files; do
     fi
 done
 
+# If there are no files to upload, exit.
+if [ -z "$upload_files" ]; then
+    echo "No files to upload."
+    exit 0
+fi
+
 upload_files=$(echo $upload_files | sed 's/ /,/g')
-curl -T "{$upload_files}" -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST
+# curl -T "{$upload_files}" -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST
+
+# If there are no files to delete, exit.
+if [ -z "$delete_files" ]; then
+    echo "No files to delete."
+    exit 0
+fi
 
 delete_files=$(echo $delete_files | sed 's/ /,/g')
-curl -Q "-DELE $delete_files" -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST
-
+# curl -Q "-DELE $delete_files" -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST
