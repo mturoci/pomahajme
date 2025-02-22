bec0e729b59856fd2eb7e84afa93e963996d23f3
Martin Turoci <martin.turoci@h2o.ai>
2025-02-22 20:48:25 +0100

    feat: Improve pagination styling.

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,39 +1,44 @@
 @if ($paginator->hasPages())
-    <nav class="flex justify-center">
-        @if (!$paginator->onFirstPage())
-            <a href="{{ $paginator->previousPageUrl() }}" aria-label="predchádzajúca strana">
-                <svg class="h-7" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clip-rule="evenodd" />
-                </svg>
-            </a>
-        @endif
-
-        @if ($paginator->currentPage() > 4)
-            <span>. . .</span>
-        @endif
-        @foreach (range(1, $paginator->lastPage()) as $i)
-            @if ($i >= $paginator->currentPage() - 2 && $i <= $paginator->currentPage() + 2)
-                @if ($i == $paginator->currentPage())
-                    <span class="border-white border-b p-[2px]">{{ $i }}</span>
-                @else
-                    <a class="p-[2px]" href="{{ $paginator->url($i) }}">{{ $i }}</a>
-                @endif
+    <nav class="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+        <div class="-mt-px flex w-0 flex-1">
+            @if (!$paginator->onFirstPage())
+                <a href="{{ $paginator->previousPageUrl() }}"
+                    class="inline-flex items-center group border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-white hover:border-primary hover:text-primary">
+                    <svg class="mr-3 size-5 group-hover:text-primary" viewBox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true" data-slot="icon">
+                        <path fill-rule="evenodd"
+                            d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    Predošlá strana
+                </a>
             @endif
-        @endforeach
-        @if ($paginator->currentPage() < $paginator->lastPage() - 3)
-            <span>. . .</span>
-        @endif
-
-        @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" aria-label="ďalšia strana">
-                <svg class="h-7" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clip-rule="evenodd" />
-                </svg>
-            </a>
-        @endif
+        </div>
+        <div class="hidden md:-mt-px md:flex">
+            @foreach (range(max(1, $paginator->currentPage() - 3), min($paginator->lastPage(), $paginator->currentPage() + 3)) as $i)
+                @if ($i == $paginator->currentPage())
+                    <a href="#"
+                        class="inline-flex items-center border-t-2 border-primary px-4 pt-4 text-sm font-medium text-primary"
+                        aria-current="page">{{ $i }}</a>
+                @else
+                    <a href="{{ $paginator->url($i) }}"
+                        class="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-white hover:border-gray-300 hover:text-gray-700">{{ $i }}</a>
+                @endif
+            @endforeach
+        </div>
+        <div class="-mt-px flex w-0 flex-1 justify-end">
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}"
+                    class="inline-flex items-center group border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-white hover:border-primary hover:text-primary">
+                    Ďalšia strana
+                    <svg class="ml-3 size-5 text-white group-hover:text-primary" viewBox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true" data-slot="icon">
+                        <path fill-rule="evenodd"
+                            d="M2 10a.75.75 0 0 1 .75-.75h12.59l-2.1-1.95a.75.75 0 1 1 1.02-1.1l3.5 3.25a.75.75 0 0 1 0 1.1l-3.5 3.25a.75.75 0 1 1-1.02-1.1l2.1-1.95H2.75A.75.75 0 0 1 2 10Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </a>
+            @endif
+        </div>
     </nav>
 @endif
