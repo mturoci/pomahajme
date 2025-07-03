b5d5648d909592520e3f122507dace05f2561487
Martin Turoci <martin.turoci@h2o.ai>
2025-07-03 14:47:53 +0200

    feat: Gallery photo detail cosmetics.

--- resources/views/gallery/image.blade.php
+++ resources/views/gallery/image.blade.php
@@ -38,20 +38,23 @@
             </div>
             
             <div class="md:w-1/4 bg-white/10 backdrop-blur-sm p-6 overflow-y-auto">
-                <div class="mb-8 flex items-center">
+                <div class="mb-2 flex items-center">
                     <a href="{{ url('/galeria/' . $campaign->id) }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
                         </svg>
                     </a>
                     <h1 class="text-2xl m-0 font-bold text-white">{{ $image->title ?? 'Fotografia' }}</h1>
+
                 </div>
-                
+                @if ($image->description)
+                    <p class="text-gray-200 ml-6">{{ $image->description }}</p>
+                @endif
                 <div class="flex items-center mt-4">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary mr-2" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clip-rule="evenodd" />
                     </svg>
-                    <span class="text-gray-400 mr-2">Album:</span>
+                    <span class="text-gray-100 mr-2">Album:</span>
                     <a href="{{ url('/galeria/' . $campaign->id) }}" class="text-primary hover:underline">
                         {{ $campaign->title }}
                     </a>
@@ -61,15 +64,9 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary mr-2" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
                     </svg>
-                    <span class="text-gray-300">{{ $campaign->campaign_date->format('d.m.Y') }}</span>
+                    <span class="text-gray-200">{{ $campaign->campaign_date->format('d.m.Y') }}</span>
                 </div>
                 
-                @if ($image->description)
-                    <div class="mt-6 border border-white/10 p-4 rounded-lg">
-                        <h3 class="text-gray-300 font-medium mb-2">Popis fotografie:</h3>
-                        <p class="text-gray-300">{{ $image->description }}</p>
-                    </div>
-                @endif
             </div>
         </div>
     </div>
