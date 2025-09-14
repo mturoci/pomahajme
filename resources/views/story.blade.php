bd7fcd92cced2cef11e8e0b127b8b590760b1953
Martin Turoci <martin.turoci@h2o.ai>
2025-09-14 20:24:17 +0200

    fix: Preserve story content newlines.

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -37,8 +37,8 @@
 
                 <div class="flex-1 p-6">
                     <h1 class="text-tertiary mt-0.5">{{ $story->title }}</h1>
-                    <div class="text-text leading-8 prose max-w-full">
-                        {{ html_entity_decode(strip_tags($story->content)) }}
+                    <div class="text-text leading-8 prose max-w-full whitespace-pre-line">
+                        {{ html_entity_decode(strip_tags(str_replace(['<br>', '<br/>', '<br />'], "\n", $story->content))) }}
                     </div>
 
                     <a href="https://ib.fio.sk/ib/transparent?a=2201942423" target="_blank" rel="noopener"
